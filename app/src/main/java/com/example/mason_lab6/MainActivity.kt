64c4087153e18ef51c6bba994eafb455e8060533
mmason415 <makaylamason32@gmail.com>
2025-04-24 20:53:10 -0400

    Completely reimplemented my Lab 1 to run on AndroidViews instead of Compose.

---
+++ app/src/main/java/com/example/mason_lab6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mason_lab6
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    // Lab 1 resubmission: Profile Card implemented in Android Views
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
